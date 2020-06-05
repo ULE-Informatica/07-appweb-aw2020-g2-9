c39f4fc35da8e27f593286ebb45226f6afcee7f5
Micaela <mpujoh00@estudiantes.unileon.es>
2020-06-05 20:01:17 +0200

    fixed búsqueda con películas.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,10 +8,10 @@ Vue.use(Vuex)
 const axios = require('axios');
 export default new Vuex.Store({
   state: {
-      username: '',
-      fullName: 'Matt Maribojoc',
-      id: 0,
-      hidden: false,
+    username: '',
+    fullName: 'Matt Maribojoc',
+    id: 0,
+    hidden: false,
     resultados: [],
     popularMovies:[],
     popularSeries:[],
@@ -26,27 +26,25 @@ export default new Vuex.Store({
       console.log('cambiando resultados');
       state.resultados= resultadosObtenidos;
     },
-    llenarResultadosSeries(state,resultadosObtenidos){
-      console.log('cambiando resultados series');
-      state.resultadosSeries= resultadosObtenidos;
+
+    añadirResultados(state,resultadosObtenidos){
+      state.resultados.concat(resultadosObtenidos);
     },
+
     llenarPopularMovies(state,resultadosObtenidos){
       
       state.popularMovies= resultadosObtenidos;
     },
+
     llenarPopularSeries(state,resultadosObtenidos){
       
       state.popularSeries= resultadosObtenidos;
     },
+
     llenarTrending(state,resultadosObtenidos){
       
       state.trending= resultadosObtenidos;
     },
-
-
-
-
-
 
     llenarListas(state,resultadosObtenidos){
       console.log('cambiando listas');
@@ -54,26 +52,29 @@ export default new Vuex.Store({
       console.log(state.listas);
       
     },
+
     llenarEtiquetas(state,resultadosObtenidos){
       console.log('cambiando etiquetas');
       state.etiquetas= resultadosObtenidos;
     },
+
     llenarPeliculas(state,resultadosObtenidos){
       console.log('cambiando etiquetas');
       state.peliculas= resultadosObtenidos;
     },
+
     llenarListasPeliculas(state,resultadosObtenidos){
       console.log('cambiando lista-pelicuals');
       state.listaPeliculas= resultadosObtenidos;
     },
 
-    pasarLogin(state){
-      
+    pasarLogin(state){      
       const token = localStorage.userToken;
       const decoded = jwtDecode(token);
       state.id= decoded.id;
       router.push("/cuenta")  
     },
+
     cambiarID(state){
       const token = localStorage.userToken;
       const decoded = jwtDecode(token);
@@ -81,10 +82,12 @@ export default new Vuex.Store({
       console.log("Cambiando"+state.id);
       state.hidden=true;
     },
+
     eliminarID(state){
       state.id= 0;
       state.hidden=false;
     },
+
     restartStates(state){
       state.id= 0,
       state.hidden= false,
@@ -110,6 +113,7 @@ export default new Vuex.Store({
         console.log(error);
       });
     },
+    
     getPopularSeries: async function({ commit } ){
       axios.get('https://api.themoviedb.org/3/tv/popular?api_key=d3500b9561bcc274c208215eeec7093b&language=es-ES&page=1')
       .then( (response) => {
@@ -120,6 +124,7 @@ export default new Vuex.Store({
         console.log(error);
       });
     },
+
     getTrending: async function({ commit } ){
       axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=d3500b9561bcc274c208215eeec7093b&language=es-ES&page=1')
       .then( (response) => {
@@ -131,8 +136,7 @@ export default new Vuex.Store({
       });
     },
 
-    getResultados: async function({ commit },titulo ){
-      
+    getResultados: async function({ commit },titulo ){      
       console.log('entre'+JSON.stringify(titulo ));
       //console.log(resultados);
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=d3500b9561bcc274c208215eeec7093b&query='+titulo)
@@ -147,14 +151,13 @@ export default new Vuex.Store({
       });
     },
 
-    getResultadosSeries: async function({ commit },titulo ){
-      
+    getResultadosSeries: async function({ commit },titulo ){      
       console.log('entre'+JSON.stringify(titulo ));
       //console.log(resultados);
         axios.get('https://api.themoviedb.org/3/search/tv?api_key=d3500b9561bcc274c208215eeec7093b&query='+titulo)
       .then( (response) => {
         const resultados = response.data.results;
-        commit('llenarResultados',resultados);
+        commit('añadirResultados',resultados);
       })
       .catch( (error) => {
         console.log(error);
@@ -182,6 +185,7 @@ export default new Vuex.Store({
         console.log(error);
       });
     },
+
     getPeliculasLista:async function({ commit } ){
       axios.get('lista/allPeliculas',{
       params: {
@@ -198,7 +202,6 @@ export default new Vuex.Store({
       });
     },
 
-
     getEtiquetas: async function({ commit } ){
       axios.get('/etiqueta')
       .then( (response) => {
@@ -210,6 +213,7 @@ export default new Vuex.Store({
         console.log(error);
       });
     },
+
     getPeliculas: async function({ commit } ){
       axios.get('/pelicula')
       .then( (response) => {
@@ -221,8 +225,6 @@ export default new Vuex.Store({
         console.log(error);
       });
     },
-
-
 
     getUsuario: async function({ commit },usuario ){
       axios.post('/login', usuario)
@@ -237,13 +239,16 @@ export default new Vuex.Store({
         console.log(error);
       });
     },
+
     setUsuario: function({ commit } ){
       console.log("CAMBIO");      
       commit('cambiarID');
     },
+
     deleteStates: function({ commit } ){
       commit('restartStates');
     },
+
     cerrarSession:  function({ commit } ){    
                    
       axios.get("/login/logout")    
