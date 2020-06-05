926673e3532feeb713cfb536851bc8740f3a23e5
Micaela <mpujoh00@estudiantes.unileon.es>
2020-06-05 14:57:47 +0200

    update inicio.

--- frontend/src/views/Inicio.vue
+++ frontend/src/views/Inicio.vue
@@ -3,18 +3,15 @@
     <v-container class="full-height mx-3" fluid>
       
       <h3 class="mt-2">Tendencia</h3>
-
       <v-row align="center" justify="center">
         <v-col class="shrink">
-          <v-sheet dark class="my-3 mr-5" elevation="8" >
+          <v-sheet dark class="mb-3 mr-5" elevation="8" >
             <v-slide-group v-model="model" class="px-3 pb-1" show-arrows>
-              <v-slide-item v-for="pelicula in trending" :key="pelicula.id">            
-                <v-card class="ma-2 mt-3" max-width=140px flat>
+              <v-slide-item v-for="pelicula in trending" :key="pelicula.id">  
+                <v-card class="ma-2 mt-3" max-width=140px flat @click="$router.push('/peli/' + pelicula.id)">
                   <v-img v-if=pelicula.poster_path :src="'https://image.tmdb.org/t/p/w500/'+pelicula.poster_path" class="align-end" 
-                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height=180px> 
-                    <router-link :to="{ name: 'Movie', params: { id_movie: pelicula.id }}">
-                      <v-card-subtitle v-text="pelicula.original_title" class="white--text"></v-card-subtitle>
-                    </router-link>
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height=180px> 
+                    <v-card-subtitle v-text="pelicula.title" class="white--text font-weight-medium"></v-card-subtitle>                    
                   </v-img>
                 </v-card>
               </v-slide-item>
@@ -24,18 +21,15 @@
       </v-row>
 
       <h3 class="mt-2">Series populares</h3>
-
       <v-row align="center" justify="center">
         <v-col class="shrink">
-          <v-sheet dark class="my-3 mr-5" elevation="8" >
+          <v-sheet dark class="mb-3 mr-5" elevation="8" >
             <v-slide-group v-model="model" class="px-3 pb-1" show-arrows>
               <v-slide-item v-for="serie in popularSeries" :key="serie.id">            
-                <v-card class="ma-2 mt-3" max-width=140px flat>
+                <v-card class="ma-2 mt-3" max-width=140px flat @click="$router.push('/serie/' + serie.id)">
                   <v-img v-if=serie.poster_path :src="'https://image.tmdb.org/t/p/w500/'+serie.poster_path" class="align-end" 
-                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height=180px> 
-                    <router-link :to="{ name: 'Serie', params: { id_serie: serie.id }}">
-                      <v-card-subtitle v-text="serie.name" class="white--text"></v-card-subtitle>
-                    </router-link>
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height=180px> 
+                    <v-card-subtitle v-text="serie.name" class="white--text font-weight-medium"></v-card-subtitle>
                   </v-img>
                 </v-card>
               </v-slide-item>
@@ -44,19 +38,16 @@
         </v-col>
       </v-row>
 
-      <h3>Películas polpulares</h3>
-
+      <h3 class="mt-2">Películas polpulares</h3>
       <v-row align="center" justify="center">
         <v-col class="shrink">
-          <v-sheet dark class="my-3 mr-5" elevation="8" >
+          <v-sheet dark class="mb-3 mr-5" elevation="8" >
             <v-slide-group v-model="model" class="px-3 pb-1" show-arrows>
               <v-slide-item v-for="pelicula in popularMovies" :key="pelicula.id">            
-                <v-card class="ma-2 mt-3" max-width=140px flat>
+                <v-card class="ma-2 mt-3" max-width=140px flat @click="$router.push('/peli/' + pelicula.id)">
                   <v-img v-if=pelicula.poster_path :src="'https://image.tmdb.org/t/p/w500/'+pelicula.poster_path" class="align-end" 
-                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height=180px> 
-                    <router-link :to="{ name: 'Movie', params: { id_movie: pelicula.id }}">
-                      <v-card-subtitle v-text="pelicula.original_title" class="white--text"></v-card-subtitle>
-                    </router-link>
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height=180px> 
+                    <v-card-subtitle v-text="pelicula.title" class="white--text font-weight-medium"></v-card-subtitle>                    
                   </v-img>
                 </v-card>
               </v-slide-item>
